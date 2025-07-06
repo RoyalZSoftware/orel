f067a65c7b17581aff82e1acbae72cf0708e957f
Alexander Panov <panov@royalzsoftware.de>
2025-07-06 18:34:28 +0200

    fix

--- init/setup.js
+++ init/setup.js
@@ -1,13 +1,19 @@
+import path, { join } from "path";
 import { sh } from "../core/utils/sh.js";
 import { configureFirewall } from "./firewall.js";
 import { setupSSH } from "./ssh.js";
 import { ensureRootAccess } from "./system.js";
 import { setupDeployUser } from "./user.js";
 import { installApt, installDocker } from "./vendor.js";
+import { fileURLToPath } from 'url';
 
-async function cleanNginx() {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+async function setupNginx() {
     await sh('rm /etc/nginx/sites-enabled/default || true');
     await sh('rm /etc/nginx/sites-available/default || true');
+    await sh(`cp ${join(__dirname, "..", "templates", "initial-nginx.conf")} /etc/nginx/nginx.conf`);
 }
 
 export async function initServer() {
@@ -20,5 +26,7 @@ export async function initServer() {
     await sh('echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections');
     await installApt("nginx", "iptables-persistent");
     await configureFirewall();
-    await cleanNginx();
+    await sh('service docker restart');
+
+    await setupNginx();
 }
