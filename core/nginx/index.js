5f54b68490a5b6fe70bb7c486dfee4feab944fef
Alexander Panov <panov@royalzsoftware.de>
2025-07-11 19:23:00 +0200

    fix: hasSSL

--- core/nginx/index.js
+++ core/nginx/index.js
@@ -26,7 +26,7 @@ export async function generateNginxConfig(appConfig, outputPath, sslEnabledDomai
     return parts[0];
   });
 
-  const services = appConfig.services.map((service) => {
+  const services = appConfig.services.filter(c => !!c.nginx?.subdomain).map((service) => {
     service.hasSSL = subDomainsWithSSL.includes(service.nginx.subdomain);
     return service;
   })
